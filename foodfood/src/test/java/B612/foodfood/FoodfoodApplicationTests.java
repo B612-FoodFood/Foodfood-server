02f5c09daecb0239d117ee8fb7170b6a03678498
Min Jeon <mulsoap0504@gmail.com>
2024-01-11 15:14:04 +0900

    chore: 프로젝트 환경 구축
     library로는 스프링 부트 3.2.1 사용함.
     dependency로는 Spring web, Lombok, Spring devtools, Thymeleaf, H2 database
     DB로는 H2 database를 사용함. 추후에 변경 가능.

---
+++ foodfood/src/test/java/B612/foodfood/FoodfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package B612.foodfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
