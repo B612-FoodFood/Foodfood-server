db1ed6e8fd2b0369d792a14b27548bdff674d463
Min Jeon <mulsoap0504@gmail.com>
2024-01-12 15:47:17 +0900

    feat: 파일 수정
    Nutrition이 잘못 들어가 있어서 삭제 및 생성자 추가

--- foodfood/src/main/java/B612/foodfood/domain/PersonalInformation.java
+++ foodfood/src/main/java/B612/foodfood/domain/PersonalInformation.java
@@ -1,19 +1,35 @@
 package B612.foodfood.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class PersonalInformation {
     @Id
     @GeneratedValue
     @Column(name = "personal_information_id")
     private Long id;
 
+    private String phoneNumber;
     private String email;
+
     @Embedded
     private Address address;
-    @Embedded
-    private Nutrition nutrition;
+
     @Embedded
     private LogIn logIn;
+
+    public PersonalInformation(String phoneNumber, String email, Address address, LogIn logIn) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.logIn = logIn;
+    }
 }
