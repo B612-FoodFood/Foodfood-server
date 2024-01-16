6ee85b0eb376d83d10e66cec9ee985addf3cb4cb
Min Jeon <mulsoap0504@gmail.com>
2024-01-16 11:10:48 +0900

    feat: 기능 수정

--- foodfood/src/main/java/B612/foodfood/domain/BodyComposition.java
+++ foodfood/src/main/java/B612/foodfood/domain/BodyComposition.java
@@ -1,10 +1,7 @@
 package B612.foodfood.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import static lombok.AccessLevel.*;
 
 @Entity
 @Getter
+@ToString(exclude = "member")
 @NoArgsConstructor(access = PROTECTED)
 public class BodyComposition {
     @Id
@@ -23,7 +21,8 @@ public class BodyComposition {
 
     private double weight; // 체중: 단위(kg)
     private double muscle; // 골격근량: 단위(kg)
-    private double bodyFat; // 체지방률: 단위(%)
+    @Setter(value = PROTECTED)
+    private Double bodyFat; // 체지방률: 단위(%)
     private LocalDate date;  // 기록 시간
 
     @ManyToOne(fetch = LAZY)
@@ -31,10 +30,12 @@ public class BodyComposition {
     @Setter(value = PROTECTED)
     private Member member;
 
-    public BodyComposition(double weight, double muscle, double bodyFat) {
+    public BodyComposition(double weight, double muscle, Double bodyFat) {
         this.weight = weight;
         this.muscle = muscle;
         this.bodyFat = bodyFat;
         date = LocalDate.now();
     }
+
+
 }
