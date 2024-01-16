7d53e085ca9d6ab677015a3c1700427a78a915e8
Min Jeon <mulsoap0504@gmail.com>
2024-01-16 11:10:03 +0900

    feat: 기능 추가

--- foodfood/src/main/java/B612/foodfood/domain/Member.java
+++ foodfood/src/main/java/B612/foodfood/domain/Member.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import static B612.foodfood.domain.Obesity.*;
+import static B612.foodfood.domain.Sex.FEMALE;
+import static B612.foodfood.domain.Sex.MALE;
 import static jakarta.persistence.CascadeType.*;
 import static jakarta.persistence.FetchType.*;
 import static lombok.AccessLevel.*;
@@ -32,6 +35,8 @@ public class Member {
     private Activity activity;
     @Enumerated
     private AccountType accountType;
+    @Enumerated
+    private Obesity obesity;
 
     @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "personal_information_id")
@@ -70,7 +75,42 @@ public class Member {
         meals.add(meal);
     }
 
+    public void addGoals(Goal goal) {
+        goal.setMember(this);
+        goals.add(goal);
+    }
+
     public void addBodyComposition(BodyComposition bodyComposition) {
+        if (bodyComposition.getBodyFat() == null) {
+            if (bodyCompositions.isEmpty()) {
+                // 회원가입시 체지방률 정보가 존재하지 않는 경우 체지방률을 0으로 설정
+                bodyComposition.setBodyFat(0D);
+            } else {
+                // 이외에 신체정보 기입 시 체지방률 정보가 없는 경우 직전의 체지방률 정보로 입력됨.(체지방률이 유지된다고 가정)
+                Double recentBodyFat =
+                        bodyCompositions.get(bodyCompositions.size() - 1).getBodyFat();
+                bodyComposition.setBodyFat(recentBodyFat);
+            }
+        }
+
+        bodyComposition.setMember(this);
+        bodyCompositions.add(bodyComposition);
+        setObesityBySex(bodyComposition);
+    }
+
+    public void addBodyComposition(double weight, double height, Double bodyFat) {
+        if (bodyFat == null) {
+            if (bodyCompositions.isEmpty()) {
+                // 회원가입시 체지방률 정보가 존재하지 않는 경우 체지방률을 0으로 설정
+                bodyFat = 0D;
+            } else {
+                // 이외에 신체정보 기입 시 체지방률 정보가 없는 경우 직전의 체지방률 정보로 입력됨.(체지방률이 유지된다고 가정)
+                bodyFat = bodyCompositions.get(bodyCompositions.size() - 1).getBodyFat();
+            }
+        }
+
+        BodyComposition bodyComposition =
+                new BodyComposition(weight, height, bodyFat);
         bodyComposition.setMember(this);
         bodyCompositions.add(bodyComposition);
     }
@@ -89,5 +129,52 @@ public class Member {
                 MemberDisease.createMemberDisease(disease);
         memberDisease.setMember(this);
         memberDiseases.add(memberDisease);
+    }
+
+    /**
+     * 비즈니스 로직
+     */
+    private void setObesityBySex(BodyComposition bodyComposition) throws IllegalStateException {
+        if (sex == MALE) {
+            setObesity(bodyComposition, 10, 20);
+        } else if (sex == FEMALE) {
+            setObesity(bodyComposition, 15, 25);
+        } else {
+            throw new IllegalStateException("""
+                    오류발생
+                    발생지점: setObesityBySex(Sex sex)
+                    발생이유: 성별이 정해지지 않았습니다.""");
+        }
+    }
+
+    private void setObesity(BodyComposition bodyComposition, int underWeightPoint, int overWeightPoint) {
+        if (bodyComposition.getBodyFat() < underWeightPoint) {
+            this.obesity = UNDER;
+        } else if (bodyComposition.getBodyFat() < overWeightPoint) {
+            this.obesity = STANDARD;
+        } else {
+            this.obesity = OVER;
+        }
+    }
+
+    /**
+     * 업데이트 로직
+     */
+
+    public void updatePassword(String password) {
+        this.personalInformation.updatePassword(password);
+    }
+
+    public void updateAddress(String city, String street, String zipcode) {
+        Address address = new Address(city, street, zipcode);
+        this.personalInformation.updateAddress(address);
+    }
+
+    public void updatePhoneNumber(String phoneNumber) {
+        this.personalInformation.updatePhoneNumber(phoneNumber);
+    }
+
+    public void updateEmail(String email) {
+        this.personalInformation.updateEmail(email);
     }
 }
