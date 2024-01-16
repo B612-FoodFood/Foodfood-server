0cc1e44818972c29ebc5aabade49926c20393393
Min Jeon <mulsoap0504@gmail.com>
2024-01-16 11:22:18 +0900

    fix: 파일 이름 변경

--- foodfood/src/main/java/B612/foodfood/exception/JoinException.java
+++
@@ -1,9 +0,0 @@
-package B612.foodfood.exception;
-
-public class JoinException extends Exception {
-    public JoinException() {}
-
-    public JoinException(String message) {
-        super(message);
-    }
-}
---
+++ foodfood/src/main/java/B612/foodfood/exception/MemberJoinException.java
@@ -0,0 +1,9 @@
+package B612.foodfood.exception;
+
+public class MemberJoinException extends Exception {
+    public MemberJoinException() {}
+
+    public MemberJoinException(String message) {
+        super(message);
+    }
+}
