5828305fe28481c971c002b4bd86501b0ac95cc2
Min Jeon <mulsoap0504@gmail.com>
2024-01-17 13:00:38 +0900

    feat: 제거

--- foodfood/src/main/java/B612/foodfood/exception/MemberJoinException.java
+++
@@ -1,9 +0,0 @@
-package B612.foodfood.exception;
-
-public class MemberJoinException extends Exception {
-    public MemberJoinException() {}
-
-    public MemberJoinException(String message) {
-        super(message);
-    }
-}
