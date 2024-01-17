4c23a60999c6368044e3b0186e69b634d97623b9
Min Jeon <mulsoap0504@gmail.com>
2024-01-17 13:00:29 +0900

    feat: exception 생성

---
+++ foodfood/src/main/java/B612/foodfood/exception/DataSaveException.java
@@ -0,0 +1,9 @@
+package B612.foodfood.exception;
+
+public class DataSaveException extends Exception{
+    public DataSaveException() {}
+
+    public DataSaveException(String message) {
+        super(message);
+    }
+}
