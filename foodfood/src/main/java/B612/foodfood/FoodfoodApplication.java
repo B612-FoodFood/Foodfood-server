1aae8c7366bd283afe9a15d897791078e4abe188
Min Jeon <mulsoap0504@gmail.com>
2024-01-22 12:34:09 +0900

    feat: JPA Date 설정

--- foodfood/src/main/java/B612/foodfood/FoodfoodApplication.java
+++ foodfood/src/main/java/B612/foodfood/FoodfoodApplication.java
@@ -2,12 +2,18 @@ package B612.foodfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(
+        basePackageClasses = {Jsr310JpaConverters.class},
+        basePackages = {"B612.foodfood.domain"}
+)
 public class FoodfoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodfoodApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodfoodApplication.class, args);
+    }
 
 }
