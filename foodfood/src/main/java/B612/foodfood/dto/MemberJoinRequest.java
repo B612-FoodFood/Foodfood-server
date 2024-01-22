aa6177efda4889f9b0e9121f6a520aea32f8586f
Min Jeon <mulsoap0504@gmail.com>
2024-01-22 12:32:31 +0900

    feat: dto 수정
    인자를 추가함

---
+++ foodfood/src/main/java/B612/foodfood/dto/MemberJoinRequest.java
@@ -0,0 +1,55 @@
+package B612.foodfood.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberJoinRequest {  // ㅋㅋ 인자수 보소
+    private String name;
+    // Sex
+    private String sex;
+    // LocalDate
+    private String birthDate;
+    // PersonalInformation
+    private String id;
+    private String password;
+    private String phoneNumber;
+    private String email;
+    // Address
+    private String city;
+    private String street;
+    private String zipCode;
+
+    private double height;
+    // BodyComposition
+    private Double weight;
+    private Double muscle;
+    private Double fat;
+    // Activity
+    private String activity;
+    // Goal
+    private String goal;
+    // AchieveBodyGoal
+    private double achieveMuscle;
+    private double achieveBodyFat;
+    // AccountType
+    private String accountType;
+
+
+    // AvoidFoods
+    private List<String> avoidFoods = new ArrayList<>();
+
+    // Disease
+    private List<String> diseases;
+
+    // Drug
+    private List<String> drugs;
+}
+
