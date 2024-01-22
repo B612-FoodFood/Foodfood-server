0ba6c7798d1bf24175a5f14749ac183e1d86ca49
Min Jeon <mulsoap0504@gmail.com>
2024-01-22 12:30:13 +0900

    feat: 회원가입 인코딩 기능 추가

--- foodfood/src/main/java/B612/foodfood/controller/api/MemberApiController.java
+++ foodfood/src/main/java/B612/foodfood/controller/api/MemberApiController.java
@@ -1,24 +1,26 @@
 package B612.foodfood.controller.api;
 
 import B612.foodfood.domain.*;
-import B612.foodfood.domain.dto.MemberJoinRequest;
+import B612.foodfood.dto.MemberJoinRequest;
+import B612.foodfood.exception.DataSaveException;
 import B612.foodfood.exception.NoDataExistException;
+import B612.foodfood.repository.AvoidFoodRepository;
+import B612.foodfood.repository.MemberRepository;
+import B612.foodfood.service.DiseaseService;
+import B612.foodfood.service.DrugService;
 import B612.foodfood.service.FoodService;
 import B612.foodfood.service.MemberService;
-import jakarta.servlet.ServletException;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import static B612.foodfood.domain.Sex.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -28,44 +30,62 @@ public class MemberApiController {
 
     private final MemberService memberService;
     private final FoodService foodService;
+    private final DiseaseService diseaseService;
+    private final DrugService drugService;
+    private final AvoidFoodRepository avoidFoodRepository;
+
+    private final BCryptPasswordEncoder encoder;
 
     /**
      * request: 유저 회원 가입 데이터 받기
      */
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody MemberJoinRequest request) throws NoDataExistException {
-
-        // LogIn
-        String id = request.getId();
-        String password = request.getPassword();
+    public ResponseEntity<String> join(@RequestBody MemberJoinRequest request) throws NoDataExistException, DataSaveException {
 
         String name = request.getName();
         Sex sex = Sex.valueOf(request.getSex());
         LocalDate birthDate = LocalDate.parse(request.getBirthDate());
-
+        // LogIn
+        LogIn logIn = new LogIn(request.getId(), encoder.encode(request.getPassword()));  // 비밀번호를 spring security를 이용해서 hashing
+        // Address
+        Address address = new Address(request.getCity(), request.getStreet(), request.getZipCode());
+        // PersonalInformation
+        PersonalInformation personalInformation = new PersonalInformation(logIn, request.getPhoneNumber(), request.getEmail(), address);
         // current body information
         double height = request.getHeight();
-        double weight = request.getWeight();
-        double muscle = request.getMuscle();
-        double fat = request.getFat();
-
-        // AchieveBodyGoal
-        double achieveMuscle = request.getAchieveMuscle();
-        double achieveBodyFat = request.getAchieveBodyFat();
-
+        BodyComposition bodyComposition = new BodyComposition(request.getWeight(), request.getMuscle(), request.getFat());
         // Activity
         Activity activity = Activity.valueOf(request.getActivity());
-
-        // AvoidFoods
-        List<Food> avoidFoods = new ArrayList<>();
-        for (String avoidFood : request.getAvoidFoods()) {
-            Food findFood = foodService.findFoodByName(avoidFood);
-            avoidFoods.add(findFood);
-        }
-
         // Goal
         BodyGoal goal = BodyGoal.valueOf(request.getGoal());
+        // AchieveBodyGoal
+        AchieveBodyGoal bodyGoal = new AchieveBodyGoal(request.getAchieveMuscle(), request.getAchieveBodyFat());
+        // AccountType
+        AccountType accountType = AccountType.valueOf(request.getAccountType());
 
-        return ResponseEntity.ok("회원가입 성공");
+        // create Member
+        Member member = new Member(name, sex, birthDate, personalInformation,
+                height, activity, goal, bodyGoal, accountType);
+
+        Long memberId = memberService.join(member);
+
+
+        // add information
+        member.addBodyComposition(bodyComposition);
+        for (String avoidFoodName : request.getAvoidFoods()) {
+            Food avoidFood = foodService.findFoodByName(avoidFoodName);
+            memberService.updateAddAvoidFood(memberId, avoidFood);
+        }
+        for (String diseaseName : request.getDiseases()) {
+            Disease findDisease = diseaseService.findDiseaseByName(diseaseName);
+            memberService.updateAddMemberDisease(memberId, findDisease);
+        }
+        for (String drugName : request.getDrugs()) {
+            Drug findDrug = drugService.findDrugByName(drugName);
+            memberService.updateAddMemberDrug(memberId, findDrug);
+        }
+
+
+        return ResponseEntity.ok("회원가입 성공" + memberId);
     }
 }
