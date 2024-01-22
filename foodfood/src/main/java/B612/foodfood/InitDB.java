df3bd8c60fc4d44da51ba387be8abacdadeaa4e3
Min Jeon <mulsoap0504@gmail.com>
2024-01-22 12:35:45 +0900

    feat: DB initializing

---
+++ foodfood/src/main/java/B612/foodfood/InitDB.java
@@ -0,0 +1,78 @@
+package B612.foodfood;
+
+import B612.foodfood.domain.*;
+import B612.foodfood.exception.DataSaveException;
+import B612.foodfood.exception.NoDataExistException;
+import B612.foodfood.repository.MealRepository;
+import B612.foodfood.service.*;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+import static B612.foodfood.domain.AccountType.USER;
+import static B612.foodfood.domain.Activity.LOT;
+import static B612.foodfood.domain.BodyGoal.MUSCLE;
+import static B612.foodfood.domain.Sex.MALE;
+
+@Component
+@RequiredArgsConstructor
+public class InitDB {
+
+    private final InitService initService;
+
+    @PostConstruct  // 빈으로 등록될 시 자동으로 실행됨
+    public void init() throws NoDataExistException, DataSaveException {
+        initService.mealInit();
+        initService.foodInit();
+        initService.diseaseInit();
+        initService.drugInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final MemberService memberService;
+        private final MealService mealService;
+        private final FoodService foodService;
+        private final DiseaseService diseaseService;
+        private final DrugService drugService;
+        // 필요시 더 추가할 것
+
+        void mealInit() throws NoDataExistException, DataSaveException {
+            double height = 172;
+            Address address = new Address("Seoul", "Hongdae", "12345");
+            LogIn logIn = new LogIn("id", "password");
+            PersonalInformation personalInformation = new PersonalInformation(logIn, "010-1234-5678", "email@gmail.com", address);
+            AchieveBodyGoal bodyGoal = new AchieveBodyGoal(35, 11);
+            LocalDate birthDate = LocalDate.of(2000, 5, 4);
+            Member member = new Member("member", MALE, birthDate, personalInformation, height, LOT, MUSCLE, bodyGoal, USER);
+
+            Long memberId = memberService.join(member);
+            Meal meal = new Meal();
+            member.addMeal(meal);
+
+            mealService.save(meal);
+
+        }
+
+        void foodInit() throws DataSaveException {
+            foodService.save(new Food("food1", new Nutrition(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)));
+            foodService.save(new Food("food2", new Nutrition(1, 1, 1, 1, 1, 1, 1, 1, 1, 1)));
+        }
+
+        void diseaseInit() throws DataSaveException {
+            diseaseService.save(new Disease("disease1"));
+            diseaseService.save(new Disease("disease2"));
+        }
+
+        void drugInit() throws DataSaveException {
+            drugService.save(new Drug("drug1"));
+        }
+    }
+}
+
