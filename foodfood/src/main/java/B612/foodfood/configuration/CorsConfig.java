f9de0e20df779d96d37b9ca9a5900d4a242014e7
Min Jeon <mulsoap0504@gmail.com>
2024-02-27 01:01:08 +0900

    feat: 이름변경

--- foodfood/src/main/java/B612/foodfood/configuration/CorsConfig.java
+++ foodfood/src/main/java/B612/foodfood/configuration/CorsConfig.java
@@ -5,7 +5,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
+    // 외부와 리소스 공유를 위한 통신을 위한 Cors 설정
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
