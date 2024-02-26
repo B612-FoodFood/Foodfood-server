701cff275bf513f7727372f9d33faa52707deecd
Min Jeon <mulsoap0504@gmail.com>
2024-02-26 23:29:12 +0900

    feat: 출시 버전 로그 기록
    Food_Food_Version_0.9 (released 2024-02-26)

--- foodfood/src/main/java/B612/foodfood/FoodfoodApplication.java
+++ foodfood/src/main/java/B612/foodfood/FoodfoodApplication.java
@@ -1,10 +1,11 @@
 package B612.foodfood;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-
+@Slf4j
 @SpringBootApplication
 @EntityScan(
         basePackageClasses = {Jsr310JpaConverters.class},
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 public class FoodfoodApplication {
 
     public static void main(String[] args) {
+        log.info("Food_Food_Version_0.9 (released 2024-02-26)");
         SpringApplication.run(FoodfoodApplication.class, args);
     }
 
