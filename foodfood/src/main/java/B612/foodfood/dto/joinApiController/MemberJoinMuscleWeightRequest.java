86d7514abf4956b8ea23686c35eca940706688c5
Min Jeon <mulsoap0504@gmail.com>
2024-02-28 21:30:32 +0900

    feat: bmi 계산 기능 추가

--- foodfood/src/main/java/B612/foodfood/dto/joinApiController/MemberJoinMuscleWeightRequest.java
+++ foodfood/src/main/java/B612/foodfood/dto/joinApiController/MemberJoinMuscleWeightRequest.java
@@ -8,5 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberJoinMuscleWeightRequest {
+    private double achieveWeight;
     private double achieveMuscle;
 }
